e4de3dc9ef6fe1efd277472f7bea2b3c05be7ea8
Wildan M <willnode@wellosoft.net>
2020-10-08 07:30:23 +0700

    Adjustment for DOM & Phusion Passenger
    + server.js > app.js
    + add deploy script
    + remove heroku specific filters
    + adjust parseURL (due to passenger bug)

--- app.js
+++ app.js
@@ -28,21 +28,10 @@ cors_proxy.createServer({
   removeHeaders: [
     'cookie',
     'cookie2',
-    // Strip Heroku-specific headers
-    'x-request-start',
-    'x-request-id',
-    'via',
-    'connect-time',
-    'total-route-time',
-    // Other Heroku added debug headers
-    // 'x-forwarded-for',
-    // 'x-forwarded-proto',
-    // 'x-forwarded-port',
   ],
   redirectSameOrigin: true,
   httpProxyOptions: {
-    // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
-    xfwd: false,
+    xfwd: true,
   },
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
